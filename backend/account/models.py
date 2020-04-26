f631cae876d579b6d37204a05f0c85580df0e3b9
Alireza Azizi <alirezaazizi1396622@gmail.com>
2020-04-26 21:33:41 +0500

    Add validator for account models fields

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.contrib.auth.models import UnicodeUsernameValidator, AbstractBaseUser, PermissionsMixin, UserManager
+from django.core.validators import RegexValidator, MinLengthValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.core.mail import send_mail
@@ -17,14 +18,17 @@ class User(AbstractBaseUser, PermissionsMixin):
         _('username'),
         max_length=150,
         unique=True,
-        help_text=_(
-            'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-        validators=[username_validator],
+        validators=[username_validator,
+                    RegexValidator(
+                        regex=r'^[0-9]{10}$',
+                        message=_(
+                            'Username must include English numbers and length of 10'),
+                    )],
         error_messages={
             'unique': _("A user with that username already exists."),
         },
     )
-    email = models.EmailField(_('email address'), blank=True, error_messages={
+    email = models.EmailField(_('email address'), unique=True, blank=True, error_messages={
         'unique': ("A user with that email already exists."),
     })
     role = models.PositiveSmallIntegerField(
@@ -68,13 +72,23 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Patient(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, unique=True, related_name="patient_profile")
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    mobile_number = models.CharField(max_length=150)
-    address = models.TextField(max_length=400)
+        User, on_delete=models.CASCADE, unique=True, related_name="patient")
+
+    first_name = models.CharField(max_length=150, validators=[
+                                  MinLengthValidator(limit_value=3)])
+
+    last_name = models.CharField(max_length=150, validators=[
+                                 MinLengthValidator(limit_value=3)])
+
+    mobile_number = models.CharField(
+        max_length=150, validators=[RegexValidator(regex=r'^091[0-9]{8}$', message="Mobile number must be 091XXXXXXXX")])
+
+    address = models.TextField(max_length=400, validators=[
+                               MinLengthValidator(limit_value=10)])
+
     birth_date = models.DateField()
-    avatar = models.ImageField(upload_to="avatar/", blank=True)
+    avatar = models.ImageField(
+        upload_to="avatar/", default="../media/avatar.png")
     gender = models.PositiveSmallIntegerField(
         choices=GENDERS)
 
@@ -84,15 +98,30 @@ class Patient(models.Model):
 
 class Doctor(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, unique=True, related_name="doctor_profile")
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    phone_number = models.CharField(max_length=150)
-    address = models.TextField(max_length=400)
+        User, on_delete=models.CASCADE, unique=True, related_name="doctor")
+
+    first_name = models.CharField(max_length=150, validators=[
+                                  MinLengthValidator(limit_value=3)])
+
+    last_name = models.CharField(max_length=150, validators=[
+                                 MinLengthValidator(limit_value=3)])
+
+    phone_number = models.CharField(max_length=150, validators=[RegexValidator(
+        regex=r'^([0-9]{3})-([0-9]{8})$', message="Phone number must include English numbers and format of XXX-XXXXXXXX ")])
+
+    address = models.TextField(max_length=400, validators=[
+                               MinLengthValidator(limit_value=10)])
+
     birth_date = models.DateField()
-    speciality = models.CharField(max_length=150)
-    bio = models.TextField(max_length=400, blank=True)
-    avatar = models.ImageField(upload_to="avatar/", blank=True)
+
+    speciality = models.CharField(max_length=150, validators=[
+                                  MinLengthValidator(limit_value=3)])
+
+    bio = models.TextField(max_length=500, blank=True)
+
+    avatar = models.ImageField(
+        upload_to="avatar/", default="../media/avatar.png")
+
     gender = models.PositiveSmallIntegerField(
         choices=GENDERS)
 
